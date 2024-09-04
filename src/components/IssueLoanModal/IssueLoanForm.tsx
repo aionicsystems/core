bcfbe3b51201dcd9e1ca7c624fd3cb70b46bcd55
Charles Dusek <dusek@onomy.io>
2024-09-04 17:07:36 -0500

    Adding in Issue Loan TX

--- src/components/IssueLoanModal/IssueLoanForm.tsx
+++ src/components/IssueLoanModal/IssueLoanForm.tsx
@@ -1,6 +1,8 @@
 import { FC, useState } from "react";
 import { Button } from "../Button/Button.tsx";
 import styles from "./IssueLoanForm.module.css";
+import { contractAddress } from "../../repository/contracts.ts";
+import { useAccount, useWriteContract } from 'wagmi';
 
 export type IssueLoanFormProps = {
   loanID?: string;
@@ -8,6 +10,8 @@ export type IssueLoanFormProps = {
 
 export const IssueLoanForm: FC<IssueLoanFormProps> = ({ loanID }) => {
   const [collateral, setCollateral] = useState<string>("0.00 ETH");
+  const { data: hash, writeContract } = useWriteContract()
+  const { chain } = useAccount();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
@@ -17,9 +21,21 @@ export const IssueLoanForm: FC<IssueLoanFormProps> = ({ loanID }) => {
     }
   };
 
+  async function submit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const formData = new FormData(e.target as HTMLFormElement)
+    const tokenId = formData.get('tokenId') as string
+    writeContract({
+      address: contractAddress("window", chain?.name),
+      abi,
+      functionName: 'mint',
+      args: [BigInt(tokenId)],
+    })
+  }
+
   return (
     <form
-      onSubmit={(event) => event.preventDefault()}
+      onSubmit={submit}
       id={loanID}
       className={styles.issueLoanForm}
     >
---
+++ src/repository/contracts.ts
@@ -0,0 +1,12 @@
+export function contractAddress(contract: string, chain: Chain | undefined, ) {
+    if (contract == "window") {
+        switch (network) {
+            case "ethereum":
+                console.log("I own a lizard");
+                break;
+            default:
+                return "0xbC873913990da5673D739be7a2C15AEbA4e651b5"
+        }
+    }
+
+}
