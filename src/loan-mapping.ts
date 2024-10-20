8bd19f824b31d9d37054fd4f14792d7d9f6c2f2c
charles <charles@aionic.systems>
2024-10-20 15:49:38 +0000

    Update subgraph.yaml and src/loan-mapping.ts

--- src/loan-mapping.ts
+++ src/loan-mapping.ts
@@ -15,21 +15,9 @@ export function handleLoanEntity(event: LoanEntityEvent): void {
   
   log.debug('The LoanID is: {} ', [event.params.loanAddress.toString()]);
 
-  loan.owner = event.params.owner
   loan.collateralAmount = event.params.collateralAmount
-  loan.asset = event.params.assetAddress
   loan.liabilityAmount = event.params.liabilityAmount
-  loan.borrowingRatio = event.params.borrowingRatio
-  loan.liquidationRatio = event.params.liquidationRatio
-  loan.interestRate = event.params.interestRate
   loan.lastCollection = event.params.lastCollection
-  loan.blockNumber = event.block.number
-  loan.blockTimestamp = event.block.timestamp
-  loan.transactionHash = event.transaction.hash
-  loan.collectorFee = event.params.collectorFee
-  loan.daoFee = event.params.daoFee
-  loan.liquidatorFee = event.params.liquidatorFee
-  loan.precision = event.params.precision
 
   loan.save()
 }
